f8678d73044398c7dab75755061360288f7cf343
xnxhjok <ylmsy8023@yeah.net>
2020-07-19 22:51:19 +0800

    feat: test

---
+++ louv_test.go
@@ -0,0 +1,10 @@
+package gorm
+
+import "testing"
+
+// run test:
+// DEBUG=false GORM_DIALECT="mysql" GORM_DSN="root:MySQL9.9@tcp(mysql57:3306)/gorm?charset=utf8&parseTime=True" go test
+
+func TestFlow(t *testing.T) {
+
+}
