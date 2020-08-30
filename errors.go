88f414259cf0ee9fb5cef02955e9c4c522b3c821
xnxhjok <ylmsy8023@yeah.net>
2020-08-30 19:36:21 +0800

    feat: update comments

--- errors.go
+++ errors.go
@@ -6,7 +6,9 @@ import (
 )
 
 var (
-	// ErrRecordNotFound returns a "record not found error". Occurs only when attempting to query the database with a struct; querying with a slice won't return this error
+	// ErrRecordNotFound returns a "record not found error".
+	// Occurs only when attempting to query the database with a struct;
+	// querying with a slice won't return this error
 	ErrRecordNotFound = errors.New("record not found")
 	// ErrInvalidSQL occurs when you attempt a query with invalid SQL
 	ErrInvalidSQL = errors.New("invalid SQL")
